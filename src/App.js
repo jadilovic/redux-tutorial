c7cfe6822719c79fc9443c733ca57fb348b5cf3d
jadilovic <adilovic79@yahoo.com>
2021-04-27 21:58:52 +0200

    adding variables in action

--- src/App.js
+++ src/App.js
@@ -5,13 +5,45 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import { createStore } from "redux";
+import { act } from "react-dom/test-utils";
+import { INCREASE, DECREASE } from "./actions";
+// store.getState()
 
+// initial store
+const initialStore = {
+  count: 0,
+  name: "john",
+};
+
+// reducer
 function App() {
   // cart setup
 
+  function reducer(state, action) {
+    console.log({ state, action });
+    if (action.type === DECREASE) {
+      console.log("this is working");
+      return { ...state, count: state.count - 1 };
+    }
+    if (action.type === INCREASE) {
+      return { ...state, count: state.count + 1 };
+    }
+    return state;
+  }
+
+  const store = createStore(reducer, initialStore);
+  console.log(store.getState());
+  store.dispatch({ type: DECREASE });
+  console.log(store.getState());
+  store.dispatch({ type: INCREASE });
+  store.dispatch({ type: INCREASE });
+  store.dispatch({ type: INCREASE });
+  console.log(store.getState());
+
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
